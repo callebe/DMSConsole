d2194cca6cba8ef4a123ca3f0fc9c4eeb022472f
callebe <callebe.barbosa@gmail.com>
2018-03-19 18:44:22 +0000

    Refresh Functions and Add Data

--- Source/main.c
+++ Source/main.c
@@ -1,5 +1,6 @@
 // -------- Include Library
 #include <stdio.h>    //Standard Library
+#include <stdlib.h> 
 #include <unistd.h>   //Used for UART
 #include <fcntl.h>    //Used for UART
 #include <termios.h>  //Used for UART
@@ -10,7 +11,11 @@
 // -------- Main Function
 int main (void){
 
-  unsigned char rx_buffer[] = "Brazillllllllllllll";
+  char LineRoutes[45] = "João Álvaro";
+  FILE *Routes;
+  unsigned char Counter;
+  char *SucessFull_fgets = 0;
+
   unsigned int Adress = 0x180;
 
   int uart0_filestream = Config_UARTS0 ();
@@ -18,11 +23,35 @@ int main (void){
   //Buzzer Touch Off
   Buzzer_Touch_Off (uart0_filestream, 0);
 
+  Routes = fopen("Routes.txt","r");
+  if(Routes == NULL){
+    printf("--> Read erro in Routes.txt - \n");
+    return 1;
 
-  //Rx_UARTS0 (uart0_filestream, &rx_buffer[0], 12);
-  //Tx_UARTS0 (uart0_filestream, &rx_buffer[0], 12);
+  }
+  Write_String (uart0_filestream, Adress, &LineRoutes[0], 5);
 
-  Write_String (uart0_filestream, Adress, &rx_buffer[0], 12);
+  getchar();
+  for (Counter = 192 ; Counter < 255; Counter++){
+    LineRoutes[0] = Counter;
+    Write_String (uart0_filestream, Adress, &LineRoutes[0], 1);
+    printf("%d\n", Counter);
+    getchar();
+
+  }
+  
+
+  // Counter = 0;
+  // while(!feof(Routes)){
+  //   SucessFull_fgets = fgets(LineRoutes, 45,Routes);
+  //   printf("--> %s\n", LineRoutes);
+  //   if(SucessFull_fgets){
+  //     Write_String (uart0_filestream, Adress, &LineRoutes[0], 10);
+
+  //   }
+
+  // }
+  
   
   //Close UARTS0  
   close(uart0_filestream);
